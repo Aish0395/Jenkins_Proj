457f9a295d2e5fd03195438c3ce6771bc510b4ac
Aishwarya Soundarya <Aishwarya Soundarya@Aishu0395>
2024-10-18 11:14:45 +0530

    First commit by Aishu

---
+++ Jenkins/src/test/java/orgModule/ToCreateOrganization.java
@@ -0,0 +1,15 @@
+package orgModule;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class ToCreateOrganization {
+	@Test
+	public void createCampaign()
+	{
+		Reporter.log("Organizaton creted successfully",true);
+	}
+	
+
+}
+
